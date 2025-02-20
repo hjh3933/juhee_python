a3ce6b6c5ee8e828634cd697ed9add2a950a2d85
hjh3933 <hkh3933@naver.com>
2025-02-20 21:43:30 +0900

    0220capPrac

--- firstMy.py
+++ firstMy.py
@@ -1,6 +1,9 @@
 # 줄 서있는 손님들의 모자 뒤집기
 cap1 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "B", "F"]
 cap2 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "F", "F"]
+cap3 = ["F", "F", "B", "H", "B", "F", "B", "B", "B", "F", "H", "F", "F"]
+emptyCap = []
+
 
 ######## 각주와 함께 보기 ########
 
@@ -92,7 +95,7 @@ def pleaseConform3(caps):
     # + 는 두 개의 리스트를 합쳐서 새로운 리스트를 만들어주는 역할을 함 [] 형태로 리스트를 합쳐야함!
     caps = caps + [caps[0]]
     for i in range(1, len(caps)):
-        # 이전 사람과 방향이 다름름
+        # 이전 사람과 방향이 다름
         if caps[i] != caps[i - 1]:
             # 첫번째 사람과 방향이 다름
             if caps[i] != caps[0]:
@@ -102,4 +105,65 @@ def pleaseConform3(caps):
                 print(" through", i - 1, "flip your caps!")
 
 
-pleaseConform3(cap1)
+# pleaseConform3(cap1)
+
+
+######## 연습해보기 ########
+def pleaseConformPrac(caps):
+    start = forward = backward = 0
+    intervals = []
+    caps = caps + ["END"]
+    for i in range(1, len(caps)):
+        if caps[start] != caps[i]:
+            intervals.append((start, i - 1, caps[start]))
+            if caps[start] == "F":
+                forward += 1
+            # elif로 조건 세부화
+            elif caps[start] == "B":
+                backward += 1
+            start = i
+    if forward < backward:
+        flip = "F"
+    else:
+        flip = "B"
+    for t in intervals:
+        # H일 경우 반복문 패스
+        if t[2] == "H":
+            continue
+        if t[2] == flip:
+            # 연습1: 시작번호와 끝번호가 다를 경우 조건문을 추가하여 그룹이 1인인지 아닌지 판단
+            if t[0] != t[1]:
+                print("People in positions", t[0], "through", t[1], "flip your caps!")
+            else:
+                print("Person at position", t[0], "flip your caps!")
+
+
+# pleaseConformPrac(cap1)
+pleaseConformPrac(cap3)
+
+
+# 연습2
+def pleaseConformOnepass(caps):
+    # 들어온 리스트가 비어있는지에 대한 검증
+    if len(caps) == 0:
+        return print("Empty List")
+    caps = caps + [caps[0]]
+    # 시작 구간을 저장
+    first = 0
+    for i in range(1, len(caps)):
+        if caps[i] != caps[i - 1]:
+            if caps[i] != caps[0]:
+                first = i
+            else:
+                # 뒷번호에 대한 조건문을 추가하여 문장 출력
+                last = i - 1
+                if last != first:
+                    print(
+                        "People in positions", first, "through", last, "flip your caps!"
+                    )
+                else:
+                    print("Person at position", first, "flip your caps!")
+
+
+# pleaseConformOnepass(cap1)
+# pleaseConformOnepass(emptyCap)
