3a870f079dda2fb0578259e7aad28b484f49daca
hjh3933 <hkh3933@naver.com>
2025-03-25 23:08:17 +0900

    0325chap1

---
+++ chap1/point2_ex.py
@@ -0,0 +1,12 @@
+# 피보나치 수열 함수
+def fibonacci(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+for i in range(8):
+    print(fibonacci(i))
---
+++ chap1/point3_ex.py
@@ -0,0 +1,23 @@
+# 사칙연산
+
+print(2 + 3)  # 5
+print(3 * 4)  # 12
+# 나누기
+print(13 // 2)  # 6
+print(13 / 2)  # 6.5
+print(11 % 3)  # 2
+# 거듭제곱 2의 3승
+print(2**3)  # 8
+# 우선순위 변경
+print((2 + 3) * 2)  # 10
+
+# 소수 연산
+print(2.3 * 3.4)  # 7.81999999 출력
+print(3 + 1.0)  # 더 정밀한 소수 자료형 4.0으로 변환
+
+# 자료형
+print(type(3))  # int
+print(type(3.5))  # float
+print(type(2 + 3j))  # complex 복소수
+print(type(True))  # bool
+print(type("abc"))  # str
