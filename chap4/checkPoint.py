70214bb81f7844924cbb94e1942441030cbd38b1
hjh3933 <hkh3933@naver.com>
2025-04-26 15:25:01 +0900

    0426chap4

--- chap4/checkPoint.py
+++ chap4/checkPoint.py
@@ -16,3 +16,59 @@ def elevator(current, path):
 
 elevator(1, [1])
 print(len(case))
+
+# check2
+# 도시이름과 인구수가 주어진 표를 보고, 도시를 3개 선택하여 인구의 합계가 500만에 가장 가까운 조합과 그 인구수를 구하시오
+cities = [
+    ("부산광역시", 3416918),
+    ("대전광역시", 1525849),
+    ("울산광역시", 1147037),
+    ("경상남도 창원시", 1044579),
+    ("경기도 부천시", 828947),
+    ("전라북도 전주시", 654963),
+    ("경기도 안양시", 565392),
+    ("경상북도 포항시", 506494),
+    ("인천광역시", 2925967),
+    ("광주광역시", 1496172),
+    ("경기도 고양시", 1068641),
+    ("경기도 성남시", 942649),
+    ("경기도 화성시", 818760),
+    ("충청남도 천안시", 652845),
+    ("경상남도 김해시", 542713),
+    ("제주시", 489202),
+    ("대구광역시", 2453041),
+    ("경기도 수원시", 1193894),
+    ("경기도 용인시", 1061440),
+    ("충청북도 청주시", 840047),
+    ("경기도 남양주시", 702545),
+    ("경기도 안산시", 650599),
+    ("경기도 평택시", 521642),
+]
+count = 3
+closest_diff = 5000000
+goal = 5000000
+resultList = []
+resultSum = 0
+
+
+# group = [(),(),()]
+def calculator(current, group):
+    global closest_diff, resultList, resultSum
+    if len(group) == 3:
+        sum = group[0][1] + group[1][1] + group[2][1]
+        diff = abs(goal - sum)
+        if closest_diff > diff:
+            closest_diff = diff
+            resultSum = sum
+            resultList = group[:]
+        return
+    for i in range(current, len(cities)):
+        group.append(cities[i])
+        calculator(i + 1, group)
+        group.pop()
+
+
+calculator(0, [])
+# 출력
+print(resultList)
+print(f"{resultSum}명")
