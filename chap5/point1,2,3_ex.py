ee31713fb443f7b937e73b8295bbc61357464a2e
hjh3933 <hkh3933@naver.com>
2025-04-30 14:46:59 +0900

    0430chap5

--- chap5/point1,2,3_ex.py
+++ chap5/point1,2,3_ex.py
@@ -18,3 +18,21 @@ for i in range(0, len(data)):
     data[i], data[min] = data[min], data[i]
 
 print(data)
+
+# 삽입정렬
+data1 = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+
+for i in range(1, len(data1)):
+    # 임시공간에 저장
+    temp = data1[i]
+    # 요소 앞에서부터 비교 시작
+    j = i - 1
+    while (j >= 0) and (data1[j] > temp):
+        # 한칸 뒤로 옮김
+        data1[j + 1] = data1[j]
+        # 앞의 요소에 대하여 반복문 실행
+        j -= 1
+    # while문 종료 후 임시값을 앞으로 저장
+    data1[j + 1] = temp
+
+print(data1)
