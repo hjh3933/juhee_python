7d68fbeb673f67873fa7de82c7e8b30c4098ecbb
hjh3933 <hkh3933@naver.com>
2025-05-14 14:27:08 +0900

    0514chap6

--- chap6/point3.md
+++ chap6/point3.md
@@ -34,4 +34,38 @@ def dijkstra(edges, num_v):
     return dist
 ```
 
+**복잡도 개선하기**
+
+- 다익스트라 알고리즘의 복잡도는 중첩 반복문을 생각하면 `O(n^2)`이다, 그러나 각 정점을 탐색하더라도 횟수는 1회뿐이므로 변의 수가 m이라고 하면 복잡도는 `O(m)`, 알고리즘 전체는 `O(m+n^2)`이 되는데 m은 최대 n(n-1)/2 이므로 복잡도는 `O(n)`이 된다
+- 우선순위가 있는 큐 자료구조(피보나치 힙)를 통해 전체 시간복잡도를 `O(m+nlogn)`으로 감소시킬 수 있다
+- 다만 피보나치 힙을 사용하면 구현이 어려워지므로 간단한 힙을 이용한 우선순위가 있는 큐를 사용한다
+- 힙 구현
+
+  - 현재노드를 가장 작은 값으로 대입하고 왼쪽, 오른쪽 자식과 대소비교후 min값을 업데이트
+  - 현재노드와 min값이 다른 경우 값을 바꿔 가장 작은 값이 현재(부모)노드에 오도록함
+  - min_heapify는 루트만 최솟값을 보장하기 때문에 dijkstra2에서 계속 큐를 재구성함
+  - 다익스트라에서는 항상 가장 작은 거리의 정점 하나만 꺼내면 되므로, 최소 힙이면 충분
+
+  ```python
+  def min_heapify(data, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    min = i
+
+    if left < len(data) and data[i][0] > data[left][0]:
+        min = left
+    if right < len(data) and data[min][0] > data[right][0]:
+        min = right
+    if min != i:
+        data[i], data[min] = data[min], data[i]
+        min_heapify(data, min)
+    #
+    def dijkstra2(edges, num_v):
+        ...
+        # 큐 재구성
+        min_heapify(q, 0)
+        ...
+        return dist
+  ```
+
 **[실습파일](point3_ex.py)**
--- chap6/point3_ex.py
+++ chap6/point3_ex.py
@@ -33,3 +33,69 @@ edges = [
 ]
 
 print(dijkstra(edges, 7))
+
+
+# 복잡도 개선 - 힙 구현
+def min_heapify(data, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    min = i
+
+    if left < len(data) and data[i][0] > data[left][0]:
+        min = left
+    if right < len(data) and data[min][0] > data[right][0]:
+        min = right
+    if min != i:
+        data[i], data[min] = data[min], data[i]
+        min_heapify(data, min)
+
+
+def dijkstra2(edges, num_v):
+    dist = [float("inf")] * num_v
+    dist[0] = 0
+    q = [[0, 0]]
+
+    while len(q) > 0:
+        q[0], q[-1] = q[-1], q[0]
+        _, u = q.pop()
+
+        # 큐 재구성
+        min_heapify(q, 0)
+
+        for i in edges[u]:
+            if dist[i[0]] > dist[u] + i[1]:
+                dist[i[0]] = dist[u] + i[1]
+                q.append((dist[u] + i[1], i[0]))
+                j = len(q) - 1
+                while (j > 0) and (q[(j - 1) // 2] > q[j]):
+                    q[(j - 1) // 2], q[j] = q[j], q[(j - 1) // 2]
+                    j = (j - 1) // 2
+
+    return dist
+
+
+print(dijkstra2(edges, 7))
+
+# 라이브러리 사용
+import heapq
+
+
+def dijkstra3(edges, num_v, goal):
+    dist = [float("inf")] * num_v
+    dist[0] = 0
+    q = []
+    heapq.heappush(q, [0, [0]])
+
+    while len(q) > 0:
+        # 힙에서 요소 꺼내기
+        _, u = heapq.heappop(q)
+        last = u[-1]
+
+        if last == goal:
+            return u
+        for i in edges[last]:
+            if dist[i[0]] > dist[last] + i[1]:
+                # 정점까지의 비용을 갱신할 수 있다면 갱신해 힙에 등록
+                dist[i[0]] = dist[last] + i[1]
+                heapq.heappush(q, [dist[last] + i[1], u + [i[0]]])
+    return []
