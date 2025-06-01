faf06b223a21d9ecfa138387d4449241536331d2
hjh3933 <hkh3933@naver.com>
2025-06-01 19:15:39 +0900

    0601chap03

--- CodingTest/03/chap03_2.py
+++ CodingTest/03/chap03_2.py
@@ -2,19 +2,19 @@
 import sys
 
 input = sys.stdin.readline
-# suNo, quizNo = map(int, input().split())
-# numbers = list(map(int, input().split()))
-# prefix_sum = [0]
-# temp = 0
+suNo, quizNo = map(int, input().split())
+numbers = list(map(int, input().split()))
+prefix_sum = [0]
+temp = 0
 
-# for i in numbers:
-#     temp += i
-#     prefix_sum.append(temp)
+for i in numbers:
+    temp += i
+    prefix_sum.append(temp)
 
-# for i in range(0, quizNo):
-#     # 질의 범위 받기
-#     s, e = map(int, input().split())
-#     print(prefix_sum[e] - prefix_sum[s - 1])
+for i in range(0, quizNo):
+    # 질의 범위 받기
+    s, e = map(int, input().split())
+    print(prefix_sum[e] - prefix_sum[s - 1])
 
 # 004
 n, m = map(int, input().split())
