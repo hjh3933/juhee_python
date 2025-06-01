b48429683f7b11c428740aec4377176f9158b351
hjh3933 <hkh3933@naver.com>
2025-06-01 19:12:55 +0900

    0601chap03

---
+++ CodingTest/chap03_2.py
@@ -0,0 +1,34 @@
+# 003
+import sys
+
+input = sys.stdin.readline
+# suNo, quizNo = map(int, input().split())
+# numbers = list(map(int, input().split()))
+# prefix_sum = [0]
+# temp = 0
+
+# for i in numbers:
+#     temp += i
+#     prefix_sum.append(temp)
+
+# for i in range(0, quizNo):
+#     # 질의 범위 받기
+#     s, e = map(int, input().split())
+#     print(prefix_sum[e] - prefix_sum[s - 1])
+
+# 004
+n, m = map(int, input().split())
+A = [[0] * (n + 1)]
+D = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(n):
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1] + A[i][j]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1])
