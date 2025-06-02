63a22b568075344d1142b5c079561613c0e6ef17
hjh3933 <hkh3933@naver.com>
2025-06-02 15:19:17 +0900

    0602chap03

--- CodingTest/03/chap03_2.py
+++ CodingTest/03/chap03_2.py
@@ -1,7 +1,8 @@
-# 003
 import sys
+import math
 
 input = sys.stdin.readline
+# 003
 suNo, quizNo = map(int, input().split())
 numbers = list(map(int, input().split()))
 prefix_sum = [0]
@@ -32,3 +33,25 @@ for i in range(1, n + 1):
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
     print(D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1])
+
+# 005
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+S = [0] * n
+C = [0] * m
+S[0] = A[0]
+answer = 0
+
+for i in range(1, n):
+    S[i] = S[i - 1] + A[i]
+
+for i in range(len(S)):
+    remainder = S[i] % m
+    if remainder == 0:
+        answer += 1
+    C[remainder] += 1
+
+for i in range(m):
+    answer += math.comb(C[i], 2)
+
+print(answer)
