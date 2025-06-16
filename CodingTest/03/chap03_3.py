998e6e1fe9b3a40b57e67624999560ed85912b3d
hjh3933 <hkh3933@naver.com>
2025-06-16 18:02:56 +0900

    0616chap03

--- CodingTest/03/chap03_3.py
+++ CodingTest/03/chap03_3.py
@@ -2,25 +2,25 @@ import sys
 
 input = sys.stdin.readline
 # 006
-# n = int(input())
-# count = 1
-# start_index = 1
-# end_index = 1
-# sum = 1
+n = int(input())
+count = 1
+start_index = 1
+end_index = 1
+sum = 1
 
-# while end_index != n:
-#     if sum == n:
-#         count += 1
-#         end_index += 1
-#         sum += end_index
-#     elif sum > n:
-#         sum -= start_index
-#         start_index += 1
-#     else:
-#         end_index += 1
-#         sum += end_index
+while end_index != n:
+    if sum == n:
+        count += 1
+        end_index += 1
+        sum += end_index
+    elif sum > n:
+        sum -= start_index
+        start_index += 1
+    else:
+        end_index += 1
+        sum += end_index
 
-# print(count)
+print(count)
 
 ### 007
 N = int(input())
@@ -42,3 +42,30 @@ while i < j:
         j -= 1
 
 print(count)
+
+### 008
+N = int(input())
+Result = 0
+A = list(map(int, input().split()))
+A.sort()
+
+for k in range(N):
+    # 변수 초기화
+    find = A[k]
+    i = 0
+    j = N - 1
+    while i < j:
+        if A[i] + A[j] == find:
+            if i != k and j != k:
+                Result += 1
+                break
+            elif i == k:
+                i += 1
+            elif j == k:
+                j -= 1
+        elif A[i] + A[j] > find:
+            j -= 1
+        else:
+            i += 1
+
+print(Result)
