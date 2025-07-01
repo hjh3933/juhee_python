89244fd8ca81f51b9f43ed2b9b91b53e85fc7b1d
hjh3933 <hkh3933@naver.com>
2025-07-01 21:14:25 +0900

    0701chap03

--- CodingTest/03/chap03_5.py
+++ CodingTest/03/chap03_5.py
@@ -34,3 +34,20 @@ for i in range(N):
 if result:
     for i in answer:
         print(i)
+
+# 012
+N = int(input())
+A = list(map(int, input().split()))
+ans = [0] * N
+myStack = []
+
+for i in range(N):
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i]
+    myStack.append(i)  # 인덱스를 저장
+
+while myStack:
+    ans[myStack.pop()] = -1
+
+for i in range(N):
+    print(ans[i], end=" ")
