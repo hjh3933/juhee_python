22dbb54feddf7849a028f8e555a89f665dd30410
hjh3933 <hkh3933@naver.com>
2025-07-08 21:18:52 +0900

    0708chap04

--- CodingTest/03/chap03_5.py
+++ CodingTest/03/chap03_5.py
@@ -6,67 +6,67 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 # 011
-# N = int(input())
-# A = [0] * N
-# for i in range(N):
-#     A[i] = int(input())
+N = int(input())
+A = [0] * N
+for i in range(N):
+    A[i] = int(input())
 
-# stack = []
-# num = 1
-# result = True
-# answer = []
+stack = []
+num = 1
+result = True
+answer = []
 
-# for i in range(N):
-#     su = A[i]
-#     if su >= num:
-#         while su >= num:
-#             stack.append(num)
-#             num += 1
-#             answer.append("+")
-#         stack.pop()
-#         answer.append("-")
-#     else:
-#         n = stack.pop()
-#         if n > su:
-#             print("NO")
-#             result = False
-#             break
-#         else:
-#             answer.append("-")
+for i in range(N):
+    su = A[i]
+    if su >= num:
+        while su >= num:
+            stack.append(num)
+            num += 1
+            answer.append("+")
+        stack.pop()
+        answer.append("-")
+    else:
+        n = stack.pop()
+        if n > su:
+            print("NO")
+            result = False
+            break
+        else:
+            answer.append("-")
 
-# if result:
-#     for i in answer:
-#         print(i)
+if result:
+    for i in answer:
+        print(i)
 
 # 012
-# N = int(input())
-# A = list(map(int, input().split()))
-# ans = [0] * N
-# myStack = []
+N = int(input())
+A = list(map(int, input().split()))
+ans = [0] * N
+myStack = []
 
-# for i in range(N):
-#     while myStack and A[myStack[-1]] < A[i]:
-#         ans[myStack.pop()] = A[i]
-#     myStack.append(i)  # 인덱스를 저장
+for i in range(N):
+    while myStack and A[myStack[-1]] < A[i]:
+        ans[myStack.pop()] = A[i]
+    myStack.append(i)  # 인덱스를 저장
 
-# while myStack:
-#     ans[myStack.pop()] = -1
+while myStack:
+    ans[myStack.pop()] = -1
 
-# for i in range(N):
-#     print(ans[i], end=" ")
+for i in range(N):
+    print(ans[i], end=" ")
 
 # 013
-# N = int(input())
-# myQueue = deque()
+N = int(input())
+myQueue = deque()
 
-# for i in range(1, N + 1):
-#     myQueue.append(i)
+for i in range(1, N + 1):
+    myQueue.append(i)
 
-# while len(myQueue) > 1:
-#     myQueue.popleft()
-#     myQueue.append(myQueue.popleft())
+while len(myQueue) > 1:
+    myQueue.popleft()
+    myQueue.append(myQueue.popleft())
 
-# print(myQueue[0])
+print(myQueue[0])
 
 # 014
 N = int(input())
---
+++ CodingTest/04/chap04_1.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+# 015
+N = int(input())
+A = [0] * N
+
+for i in range(N):
+    num = int(input())
+    A[i] = num
+
+for i in range(N - 1):
+    for j in range(N - 1 - i):
+        if A[j] > A[j + 1]:
+            temp = A[j]
+            A[j] = A[j + 1]
+            A[j + 1] = temp
+
+for i in range(N):
+    print(A[i])
