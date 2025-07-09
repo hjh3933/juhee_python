33a451ef3065629e0d1fee25939c18bd810512b6
hjh3933 <hkh3933@naver.com>
2025-07-09 20:52:56 +0900

    0709chap04

--- CodingTest/04/chap04_1.py
+++ CodingTest/04/chap04_1.py
@@ -3,19 +3,36 @@ import sys
 input = sys.stdin.readline
 
 # 015
+# N = int(input())
+# A = [0] * N
+
+# for i in range(N):
+#     num = int(input())
+#     A[i] = num
+
+# for i in range(N - 1):
+#     for j in range(N - 1 - i):
+#         if A[j] > A[j + 1]:
+#             temp = A[j]
+#             A[j] = A[j + 1]
+#             A[j + 1] = temp
+
+# for i in range(N):
+#     print(A[i])
+
+# 016
 N = int(input())
-A = [0] * N
+A = []
 
 for i in range(N):
-    num = int(input())
-    A[i] = num
+    # (값, 인덱스)
+    A.append((int(input()), i))
 
-for i in range(N - 1):
-    for j in range(N - 1 - i):
-        if A[j] > A[j + 1]:
-            temp = A[j]
-            A[j] = A[j + 1]
-            A[j + 1] = temp
+Max = 0
+sorted_A = sorted(A)
 
 for i in range(N):
-    print(A[i])
+    if sorted_A[i][1] - i > Max:
+        Max = sorted_A[i][1] - i
+
+print(Max + 1)
